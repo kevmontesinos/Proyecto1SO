02d2f8d2b1f4bc9b10d3ca101091cf6d8f36d089
kevmontesinos <kevin.montesinos@correo.unimet.edu.ve>
2023-02-23 07:55:40 -0400

    archivo constante

---
+++ src/Utils/ConstantsK.java
@@ -0,0 +1,40 @@
+package Utils;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Jose Rubin
+ */
+public class ConstantsK {
+    //Constante Tiempo
+
+    public static int tiempoDia;
+
+    //Constantes Rubin
+    public static volatile int countdownKev = 30;
+    public static int maxWorkersKev = 15;
+
+    public static int numProducerIntroKev;
+    public static int numProducerPTKev;
+    public static int numProducerClosureKev;
+    public static int numProducerCreditsKev;
+    public static int numProducerStartKev;
+
+    public static volatile int driveMaxIntroKev;
+    public static volatile int driveMaxInicioKev;
+    public static volatile int driveMaxCreditosKev;
+    public static volatile int driveMaxCierreKev;
+    public static volatile int driveMaxPlotTwistKev;
+
+    //Dinero Constantes
+    public static int promedioGastosKev;
+    public static int promedioGananciasKev;
+    public static int promedioCapsGananciasKev;
+    public static int numTotalCapsKev;
+    public static int promedioPerdidaJefesKev;
+
+}
